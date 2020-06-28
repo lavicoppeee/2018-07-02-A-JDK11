87e141b8fc2a82c52d7b8832b2039755e9bd8da2
lavic98 <lavic98@macbook-pro-2.station>
2020-06-28 17:53:16 +0200

    FINAL 1.0

--- src/main/java/it/polito/tdp/extflightdelays/model/Model.java
+++ src/main/java/it/polito/tdp/extflightdelays/model/Model.java
@@ -34,7 +34,6 @@ public class Model {
 		dao.getVertici(idMap);
 		//Graphs.addAllVertices(graph, idMap.values());
 		
-		
 		for (Arco a : dao.getArchi(x,idMap)) {
 			if (!this.graph.containsEdge(a.getA1(), a.getA2())){
 				Graphs.addEdgeWithVertices(graph, a.getA1(), a.getA2(), a.getPeso());
@@ -78,11 +77,12 @@ public class Model {
 	public List<Airport> cammino (int xMax,Airport partenza) {
 		this.bestPeso=0.0;
 		this.bestCammino=new ArrayList<>();
-		
+		this.bestCammino.add(partenza);
+
 		List<Airport> parziale=new ArrayList<Airport>();
 		parziale.add(partenza);
 		
-		ricorsione(parziale, 1, xMax);
+		ricorsione(parziale,0, xMax);
 		
 		return bestCammino;
 	}
@@ -91,41 +91,35 @@ public class Model {
 	
 	
 	
-	private void ricorsione(List<Airport> parziale, int l, double xMax) {
+	private void ricorsione(List<Airport> parziale, double l, double xMax) {
 		// TODO Auto-generated method stub
         Airport last = parziale.get(parziale.size()-1);
 		
       //caso terminale 
-        //1
-		if(l > this.bestCammino.size()) {
+        
+		    if(parziale.size() > this.bestCammino.size() || l==xMax || (parziale.size()== this.bestCammino.size() && l> this.bestPeso)) {
 			this.bestCammino = new ArrayList<>(parziale);
-			this.bestPeso = xMax;
-		}
-		//2
-		if(l == this.bestCammino.size() && xMax> this.bestPeso) {
-			this.bestCammino = new ArrayList<>(parziale);
-			this.bestPeso = xMax;
+			this.bestPeso = l;
 		}
 	
-		//ricorsione, aggiorno il peso 
+		//ricorsione, aggiorno il peso dell'a
 	List<Airport> vicini=Graphs.neighborListOf(this.graph, last);
 		
 		for(Airport v : vicini) {
 			Double peso = this.graph.getEdgeWeight(this.graph.getEdge(last, v));
-			if(!parziale.contains(v) && xMax >= peso) {
+			if(!parziale.contains(v) && (xMax-l) >= peso) {
 				parziale.add(v);
-				xMax -= peso;
-				this.ricorsione(parziale, l++, xMax);
+				l += peso;
+				this.ricorsione(parziale, l, xMax);
 				
 				parziale.remove(parziale.size()-1);
-				xMax += peso;
+				l -= peso;
 			}
 		}
    
 	}
 
-	
-	
+		
 	public Double getBestPeso() {
 		return bestPeso;
 	}
